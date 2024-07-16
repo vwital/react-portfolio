9f2d2145cc903b23b1ca8f7717d643f0a6b226e4
vwital <wital128@tut.by>
2024-07-17 02:17:13 +0300

    feat: add build

--- src/utils/detectDarkMode.js
+++
@@ -1,9 +0,0 @@
-export default function detectDarkMode() {
-  if (
-    window.matchMedia &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-  ) {
-    return 'dark';
-  }
-  return 'light';
-}
